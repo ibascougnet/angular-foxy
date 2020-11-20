5e21829640669d03bdc94de260b96ce6ba0d2542
Ignacio Bascougnet <nachobascougnet@gmail.com>
2020-11-20 13:00:30 -0300

    initial foxy commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,36 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HelloComponent } from './hello.component';
+import { BeerListComponent } from './beer-list/beer-list.component';
+
+import { FormsModule } from '@angular/forms';
+import { FoxyAboutComponent } from './foxy-about/foxy-about.component';
+import { FoxyBeersComponent } from './foxy-beers/foxy-beers.component';
+import { CartComponent } from './cart/cart.component';
+import { AppRoutingModule } from './app-routing.module';
+import { InputIntegerComponent } from './input-integer/input-integer.component';
+import { WineListComponent } from './wine-list/wine-list.component';
+import { FoxyHomeinfoComponent } from './foxy-homeinfo/foxy-homeinfo.component';
+
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
-  declarations: [ AppComponent, HelloComponent ],
-  bootstrap:    [ AppComponent ]
+  declarations: [
+    AppComponent,
+    BeerListComponent,
+    FoxyAboutComponent,
+    FoxyBeersComponent,
+    CartComponent,
+    InputIntegerComponent,
+    WineListComponent,
+    FoxyHomeinfoComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    AppRoutingModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule { }
